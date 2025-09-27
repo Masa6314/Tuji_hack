85deb95e9dfcf20a74050bf93a4d2161b2fb260f
Masa6314 <masa63141108@yahoo.co.jp>
2025-09-27 18:39:27 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,131 @@
+import os
+from datetime import datetime
+from flask import Flask, request, abort, jsonify, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+from datetime import datetime
+
+load_dotenv()  # .env を読み込む
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///local.db")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+class FormResponse(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    submitted_at = db.Column(db.DateTime, nullable=False)
+    Q1 = db.Column(db.String, nullable=True)
+    Q2 = db.Column(db.String, nullable=True)
+    Q3 = db.Column(db.String, nullable=True)
+    Q4 = db.Column(db.String, nullable=True)
+    Q5 = db.Column(db.String, nullable=True)
+    Q6 = db.Column(db.String, nullable=True)
+    # Q7 = db.Column(db.String, nullable=True)
+    # Q8 = db.Column(db.String, nullable=True)
+    # Q9 = db.Column(db.String, nullable=True)
+    # Q10 = db.Column(db.String, nullable=True)
+    # Q11 = db.Column(db.String, nullable=True)
+    # Q12 = db.Column(db.String, nullable=True)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    body = db.Column(db.String(300), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
+
+
+
+@app.route("/api/forms/google", methods=["POST"]) #POSTでアクセスが来たときに呼ばれる関数
+def receive_google_form():
+    # 共有シークレットで簡易認証
+    if request.headers.get("X-Webhook-Token") != os.getenv("WEBHOOK_TOKEN"):
+        abort(401, "invalid token")
+    body = request.get_json(silent=True) or {}
+    responses = body.get("responses")
+    ts = body.get("submitted_at")
+    if not responses:
+        abort(400, "no responses")
+
+    # 日時パース（Z終端なら除去）
+    if ts:
+      ts = ts.replace("Z", "")
+      submitted_at = datetime.fromisoformat(ts)
+    else:
+      submitted_at = datetime
+
+    rec = FormResponse(
+        submitted_at=submitted_at,
+        Q1=responses["Test 1"][0],
+        Q2=responses["Test 2"][0],
+        Q3=responses["Test 3"][0],
+        Q4=responses["Test 4"][0],
+        Q5=responses["Test 5"][0],
+        Q6=responses["Test 6"][0],
+        # Q7=responses["Test 7"][0],
+        # Q8=responses["Test 8"][0],
+        # Q9=responses["Test 9"][0],
+        # Q10=responses["Test 10"][0],
+        # Q11=responses["Test 11"][0],
+        # Q12=responses["Test 12"][0],
+    )
+    db.session.add(rec)
+    db.session.commit()
+    return jsonify({"ok": True})
+
+
+@app.route("/", methods=["GET", "POST"])
+def hello():
+    if request.method=="GET":
+        posts = Post.query.all()
+        forms = FormResponse.query.all()
+        for f in forms:
+            if f.Q1 == "選択肢 1" or "選択肢2":
+                f.Q1 = 1
+            else:
+                f.Q1 = 0
+        return render_template('index.html', posts=posts, forms=forms)
+
+
+@app.route("/create", methods=["GET", "POST"])
+def create():
+    if request.method == "POST":
+        title = request.form.get("title")
+        body = request.form.get("body")
+
+        post = Post(title=title, body=body)
+        
+        db.session.add(post)
+        db.session.commit()
+        return redirect("/")
+
+    else:
+        return render_template('create.html')
+
+@app.route("/<int:id>/update", methods=["GET", "POST"])
+def update(id):
+    post = Post.query.get(id)
+    if request.method == "GET":
+        return render_template("update.html", post=post)
+    else:
+        post.title = request.form.get("title")
+        post.body = request.form.get("body")
+
+        db.session.commit()
+        return redirect("/")
+
+
+@app.route("/<int:id>/delete", methods=["GET"])
+def delete(id):
+    post = Post.query.get(id)
+
+    db.session.delete(post)
+    db.session.commit()
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()  # 初回テーブル作成
+    app.run(host="0.0.0.0", port=8000, debug=True)
